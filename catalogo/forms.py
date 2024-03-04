bd964876a90f99380651f76469d4e1ef3a5458bd
Cristian <cksolocristian7@gmail.com>
2024-03-04 00:43:09 -0600

    The last version

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -4,4 +4,4 @@ from .models import Catalogue
 class CatalogueForm(forms.ModelForm):
     class Meta:
         model = Catalogue
-        fields = ['name_active', 'areas']
+        fields = '__all__'
